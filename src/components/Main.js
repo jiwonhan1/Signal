922896c22b9af59f83668a3c85bba4bc4bc138eb
Jiwon <jiwon1.han@gmail.com>
2020-05-17 15:37:31 -0700

    launched map

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { View, Text, StyleSheet, StatusBar } from 'react-native';
 import { fetchAreas } from "../actions/fetchData";
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 class Main extends Component {
   componentDidMount() {
@@ -14,9 +15,15 @@ class Main extends Component {
     const { error, loading, areas} = this.props;
     if(!loading) {
       return (
-      <View>
-          {areas.length ? areas.map((area, i) => <Text key={i}>{area.name}</Text>) : <Text>No area</Text>}
-      </View>
+        <View styles={styles.mapContainer}>
+        <MapView provider={PROVIDER_GOOGLE}
+     style={styles.map}
+     region={{
+       latitude: 41.89193,
+       longitude: 12.51133,
+       latitudeDelta: 0.015,
+       longitudeDelta: 0.0121}}/>
+  </View>
       );
   } else {
       return (
@@ -28,6 +35,17 @@ class Main extends Component {
 }
   
 }
+
+const styles = StyleSheet.create({
+  mapContainer : {
+     width : '100%',
+     height : 200
+   },
+  map : {
+     width : '100%',
+     height : '100%'
+   }
+ });
 const mapStateToProps = state => ({
   areas: state.areas,
   loading: state.loading,
